3025f2204f7a20ab1e577d6ff037c1d60b93fcf0
ADITYA BHARDWAJ <adityabhardwajpcm@gmail.com>
2026-02-21 16:22:19 +0530

    Add one's complement program in assembly
    Implemented a program to calculate the one's complement of a number in assembly language.

---
+++ onesComplement.asm
@@ -0,0 +1,28 @@
+; Write a program in assembly language to find one's complement of a number
+
+; Name - ADITYA BHARDWAJ
+; Section - D2
+; Roll No - 07
+; Course – B TECH
+; Branch – CSE
+
+.model small
+.stack 100h
+
+.data
+    num db 05h
+    result db ?
+
+.code
+main:
+    mov ax, @data
+    mov ds, ax
+
+    mov al, num
+    not al
+    mov result, al
+
+    mov ah, 4ch
+    int 21h
+
+end main
