27151e408234334cff509c1b8f4dbe395c836d9c
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-03-11 18:54:46 +0530

    subtractTwo8BitNumbers.asm

---
+++ subtractTwo8BitNumbers.asm
@@ -0,0 +1,11 @@
+; Write an 8085 program to subtract two 8-bit numbers.
+; Name - ADITYA BHARDWAJ
+; Section - D2
+; Roll No - 07
+; Course - B TECH
+; Branch - CSE
+
+MVI A, 5AH
+MVI B, 27H
+SUB B
+HLT
