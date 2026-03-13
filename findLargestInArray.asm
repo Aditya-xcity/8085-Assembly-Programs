ba2e883e82ea04f1d3f1985f79419dd0315446ae
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-03-13 20:11:58 +0530

    Add program: Find largest number in array (8085 Assembly)

---
+++ findLargestInArray.asm
@@ -0,0 +1,32 @@
+; Write an 8085 program to find the largest number in an array.
+; Array length at 2500H, elements start at 2501H, result stored at 2600H
+; Name - ADITYA BHARDWAJ
+; Section - D2
+; Roll No - 07
+; Course - B TECH
+; Branch - CSE
+
+LXI H, 2500H
+MOV C, M
+INX H
+
+MOV A, C
+ORA A
+JZ DONE
+
+MOV A, M
+DCR C
+JZ STORE
+
+LOOP: INX H
+      CMP M
+      JC UPDATE
+      JMP NEXT
+
+UPDATE: MOV A, M
+
+NEXT: DCR C
+      JNZ LOOP
+
+STORE: STA 2600H
+DONE:  HLT
